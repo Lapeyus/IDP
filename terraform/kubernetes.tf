707e5fa80c8ab43aff53e90fcbd0e2c0b2159515
Joseph Villarreal Lopez <lapeyus@gmail.com>
2022-09-02 17:04:35 -0600

    done

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -15,26 +15,66 @@ resource "kubernetes_secret" "git-creds" {
   metadata {
     name      = "git-creds"
     namespace = each.key
+    labels = {
+      "argocd.argoproj.io/secret-type" = "repository"
+    }
   }
   data = {
     "ssh" = file("~/.ssh/id_rsa")
+    "sshPrivateKey" = file("~/.ssh/id_rsa")
+    "url" = "git@github.com:Lapeyus/IDP.git"
   }
   depends_on = [
     google_container_cluster.primary
   ]
 
+
   for_each = toset(["config-management-system", "default"])
 }
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = google_container_cluster.primary.endpoint
-#     client_certificate     = base64decode(google_container_cluster.primary.master_auth.0.client_certificate)
-#     client_key             = base64decode(google_container_cluster.primary.master_auth.0.client_key)
-#     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-#     token                  = data.template_file.access_token.rendered
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    client_certificate     = base64decode(google_container_cluster.primary.master_auth.0.client_certificate)
+    client_key             = base64decode(google_container_cluster.primary.master_auth.0.client_key)
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+    token                  = data.template_file.access_token.rendered
+  }
+}
+
+
+#  argo helm chart.
+resource "helm_release" "argo" {
+  atomic                     = false
+  chart                      = "argo-cd"
+  cleanup_on_fail            = false
+  create_namespace           = true
+  dependency_update          = false
+  disable_crd_hooks          = false
+  disable_openapi_validation = false
+  disable_webhooks           = false
+  force_update               = true
+  lint                       = false
+  max_history                = 0
+  name                       = "argo-cd"
+  namespace                  = "default"
+  pass_credentials           = false
+  recreate_pods              = false
+  render_subchart_notes      = true
+  replace                    = false
+  repository                 = "https://argoproj.github.io/argo-helm"
+  reset_values               = false
+  reuse_values               = false
+  skip_crds                  = false
+  timeout                    = 300
+  verify                     = false
+  version                    = "4.9.10"
+  wait                       = true
+  wait_for_jobs              = false
+  values = [
+    "${file("manifests/argo-values.yaml")}"
+  ]
+}
 
 # # note this requires the terraform to be run regularly
 # resource "time_rotating" "mykey_rotation" {
@@ -121,19 +161,6 @@ resource "kubernetes_secret" "git-creds" {
 #   #   templatefile("../helm/helm-gcp-secrets/values.yaml", {
 #   #     projectID  = var.gcp_project
 #   #   })
-#   # ]
-# }
-
-# #  argo helm chart.
-# resource "helm_release" "argo" {
-#   chart            = "argo-cd"
-#   repository       = "https://argoproj.github.io/argo-helm"
-#   name             = "argo-cd"
-#   namespace        = "default"
-#   force_update     = true
-#   create_namespace = true
-#   # values = [
-#   #   "${file("manifests/argo-values.yaml")}"
 #   # ]
 # }
 
