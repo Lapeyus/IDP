58e182ed9a5ed679404edfa5a9d19a3457339acd
Joseph Villarreal Lopez <lapeyus@gmail.com>
2022-09-07 16:30:58 -0600

    x

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,4 +6,26 @@ resource "google_project_service" "gcp_resource_manager_api" {
   disable_dependent_services = true
 }
 
+# #Sleep for 2m to allow for enablement before proceeding
+# resource "time_sleep" "gcp_wait_crm_api_enabling" {
+#   depends_on = [
+#     google_project_service.gcp_resource_manager_api
+#   ]
+#   create_duration = "2m"
+# }
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "secret-version"
+  labels = {
+    label = "my-label"
+  }
+  replication {
+    automatic = true
+  }
+}
 
+
+resource "google_secret_manager_secret_version" "secret-version-basic" {
+  secret = google_secret_manager_secret.secret-basic.id
+
+  secret_data = "secret-data"
+}
