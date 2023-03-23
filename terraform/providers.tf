c0f344faa6930f05b636dd40d2a6702ab20e1c82
Joseph Villarreal Lopez <joseph.villarreal@66degrees.com>
2023-03-23 14:51:07 -0600

    feat(terraform): Add providers for GCP and Kubernetes
    - Add provider for Google Cloud Platform
    - Add provider for Kubernetes
    - Expose access token for communicating with GKE cluster

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -27,3 +27,27 @@ provider "google-beta" {
 provider "google" {
   project = var.gcp_project
 }
+
+#Expose an access token for communicating with the GKE cluster.
+data "google_client_config" "client" {}
+
+#Create access token
+data "template_file" "access_token" {
+  template = data.google_client_config.client.access_token
+}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.template_file.access_token.rendered
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    client_certificate     = base64decode(google_container_cluster.primary.master_auth.0.client_certificate)
+    client_key             = base64decode(google_container_cluster.primary.master_auth.0.client_key)
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+    token                  = data.template_file.access_token.rendered
+  }
+}
