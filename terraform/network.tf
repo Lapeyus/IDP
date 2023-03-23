f24f517e206294e535d68cc9e170bc70b04aac10
Joseph Villarreal Lopez <joseph.villarreal@66degrees.com>
2023-03-23 14:50:30 -0600

    docs(commit): Add best practices for writing commit messages
    - Add a list of best practices for writing commit messages to the documentation

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,23 +1,25 @@
-#Create new VPC for IDP Cluster
+# This resource block creates a VPC network for the IDP cluster
 resource "google_compute_network" "vpc" {
-  project                         = var.gcp_project
-  description                     = "VPC for IDP Cluster"
-  name                            = "idp"
-  auto_create_subnetworks         = "false"
-  delete_default_routes_on_create = "false"
-  mtu                             = 1460
-  routing_mode                    = "REGIONAL"
+  project                         = var.gcp_project       # The GCP project where the network will be created
+  description                     = "VPC for IDP Cluster" # A description of the network
+  name                            = "idp"                 # The name of the VPC network
+  auto_create_subnetworks         = "false"               # Disables automatic subnetwork creation
+  delete_default_routes_on_create = "false"               # Retains default routes when creating custom routes
+  mtu                             = 1460                  # The maximum transmission unit (MTU) size for the network
+  routing_mode                    = "REGIONAL"            # Specifies that the network is regional
 }
 
-#Create new subnet with two secondary ranges for pods and services
+# Define a subnetwork resource for GKE cluster
 resource "google_compute_subnetwork" "gke" {
   name                       = "gke-subnet"
   region                     = var.gcp_region
-  network                    = google_compute_network.vpc.name
-  ip_cidr_range              = var.subnetwork_cidr
-  private_ip_google_access   = true
-  private_ipv6_google_access = true
+  network                    = google_compute_network.vpc.name # Use the VPC network created earlier
+  ip_cidr_range              = var.subnetwork_cidr             # Define IP range for the subnetwork
+  private_ip_google_access   = true                            # Enable Private Google Access for instances in this subnet
+  private_ipv6_google_access = "DISABLE_GOOGLE_ACCESS"         # Disable IPv6 Google access
   project                    = var.gcp_project
+
+  # Define secondary IP ranges for pods and services
   secondary_ip_range {
     range_name    = "gke-pods-range"
     ip_cidr_range = var.pod_cidr
@@ -26,12 +28,15 @@ resource "google_compute_subnetwork" "gke" {
     range_name    = "gke-services-range"
     ip_cidr_range = var.services_cidr
   }
+
+  # Configure logging for the subnetwork
   log_config {
-    aggregation_interval = "INTERVAL_15_MIN"
-    flow_sampling        = 0.5
-    metadata             = "INCLUDE_ALL_METADATA"
+    aggregation_interval = var.aggregation_interval
+    flow_sampling        = var.flow_sampling
+    metadata             = var.metadata
   }
 }
+
 
 # resource "google_compute_firewall" "gke-net-firewall" {
 #   project  = var.gcp_project
