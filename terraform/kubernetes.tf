797f41d549ec594f66219ab1ac90e1a1fa044567
Joseph Villarreal Lopez <joseph.villarreal@66degrees.com>
2023-03-23 14:49:27 -0600

    chore(terraform): update import command for kubernetes_manifest.connector_config
    Update import command for kubernetes_manifest.connector_config resource to match current configuration

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,18 +1,4 @@
-#Expose an access token for communicating with the GKE cluster.
-data "google_client_config" "client" {}
-
-#Create access token
-data "template_file" "access_token" {
-  template = data.google_client_config.client.access_token
-}
-
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.primary.endpoint}"
-  token                  = data.template_file.access_token.rendered
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-}
-
-# # terraform import kubernetes_manifest.connector_config "apiVersion=core.cnrm.cloud.google.com/v1beta1,kind=ConfigConnector,namespace=default,name=configconnector.core.cnrm.cloud.google.com"
+# terraform import kubernetes_manifest.connector_config "apiVersion=core.cnrm.cloud.google.com/v1beta1,kind=ConfigConnector,namespace=default,name=configconnector.core.cnrm.cloud.google.com"
 resource "kubernetes_manifest" "connector_config" {
   manifest = {
     "apiVersion" = "core.cnrm.cloud.google.com/v1beta1"
@@ -47,11 +33,11 @@ resource "kubernetes_secret" "git-creds" {
     google_container_cluster.primary,
     google_gke_hub_feature.feature,
     google_gke_hub_membership.membership,
-    google_gke_hub_feature_membership.feature_member,
-    google_service_account_iam_binding.workloadIdentityUser
+    # google_gke_hub_feature_membership.feature_member2
   ]
 
 
   for_each = toset(["default", "config-management-system"])
+  # for_each = toset(["default"])
 }
 
