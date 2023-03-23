1f5e259c6745780f1afe2c775c6fc1a180e20b83
Joseph Villarreal Lopez <joseph.villarreal@66degrees.com>
2023-03-23 14:48:31 -0600

    refactor(iam): Update iam.tf
    - Remove unused dependencies
    - Update resource and variable names for consistency

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,26 +1,24 @@
-# # terraform import google_service_account.idp-robot projects/crisp-build/serviceAccounts/idp-robot@crisp-build.iam.gserviceaccount.com
+# Create the service account with display name
 resource "google_service_account" "idp-robot" {
   account_id   = "devops-robot"
   display_name = "devops-robot"
 }
+
+# Bind the owner role to the service account
 resource "google_project_iam_binding" "idp-robot" {
   project = var.gcp_project
   role    = "roles/owner"
-  members = [
-    "serviceAccount:${google_service_account.idp-robot.email}"
-  ]
+
+  members = ["serviceAccount:${google_service_account.idp-robot.email}"]
 }
 
-# " binding between the SA & the predefined Kubernetes service account that Config Connector runs"
-resource "google_service_account_iam_binding" "workloadIdentityUser" {
+# Bind workloadIdentityUser role to the Kubernetes service account
+resource "google_service_account_iam_binding" "workload_identity_user" {
   service_account_id = google_service_account.idp-robot.id
   role               = "roles/iam.workloadIdentityUser"
-  members = [
-    "serviceAccount:${var.gcp_project}.svc.id.goog[cnrm-system/cnrm-controller-manager]"
-  ]
+  members            = ["serviceAccount:${var.gcp_project}.svc.id.goog[cnrm-system/cnrm-controller-manager]"]
   depends_on = [
     google_container_cluster.primary,
-    google_service_account.idp-robot,
     google_project_iam_binding.idp-robot
   ]
 }
